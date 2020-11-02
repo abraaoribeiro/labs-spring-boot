1cee98677589a2398d901716382a71b2bbd34d77
abraao.ribeiro <abraao.ribeiro@tre-pa.jus.br>
2020-11-02 17:23:44 -0300

    Restringindo o acesso aos endpoints privados

---
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,36 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	//Configuracoes de autentição
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
+	}
+	
+	//Configuracoes de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
+	}
+	
+	//Configuracoes de recursos estaticos(js, imagens, etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+	}
+}
